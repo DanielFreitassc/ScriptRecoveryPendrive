469fd231c436548e60737456531316f4df4f5893
danielfreitassc <danielfreitassc@gmail.com>
2024-01-19 07:01:22 -0300

    commit bomba

---
+++ Praticas JS puro/Loops.js
@@ -0,0 +1,9 @@
+for(let i = 1; i < 11; i++){
+    console.log(`Contagem ${i}`)
+
+    if(i == 10){
+        for(let j = 10; j > 0; j--){
+            console.log(`Contagem ${j}`)
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Aprendendo Javascript
+# Estudos de Javascript com um pouco de HTML
+### Fiz este repositorio só para estudos de conceitos de javascript não farei nenhum projeto grande aqui.
