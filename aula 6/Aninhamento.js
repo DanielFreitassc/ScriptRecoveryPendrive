97e4ee797948033101520e7af467d8fac049d981
danielfreitassc <danielfreitassc@gmail.com>
2024-01-20 02:08:06 -0300

    mais projetos cansados

---
+++ aula 6/Aninhamento.js
@@ -0,0 +1,19 @@
+var nome = "daniel"
+var idade = 20
+
+if(nome == "daniel"){
+    console.log(`${nome} Passou por aqui`)
+    if(idade == 20){
+        console.log(`${nome} Passou por aqui e ele tem ${idade} Anos`)
+    }else if(idade == 21){
+        console.log(`${nome} Passou por aqui e ele tem ${idade} Anos`)
+    }
+
+}else if(nome == "luiz"){
+    console.log(`${nome} Passou por aqui`)
+    if(idade == 20){
+        console.log(`${nome} Passou por aqui e ele tem ${idade} Anos`)
+    }else if(idade == 21){
+        console.log(`${nome} Passou por aqui e ele tem ${idade} Anos`)
+    }
+}
