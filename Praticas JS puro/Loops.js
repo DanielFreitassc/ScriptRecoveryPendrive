5751ce0833ba89e288e84dbb2210e072a07581b2
danielfreitassc <danielfreitassc@gmail.com>
2024-05-05 23:12:09 -0300

    First commit

--- Praticas JS puro/Loops.js
+++
@@ -1,9 +0,0 @@
-for(let i = 1; i < 11; i++){
-    console.log(`Contagem ${i}`)
-
-    if(i == 10){
-        for(let j = 10; j > 0; j--){
-            console.log(`Contagem ${j}`)
-        }
-    }
-}
